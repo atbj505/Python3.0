db56bd51e65d5c0624f79bb545b0835728686c69
atbj505 <atbj505@hotmail.com>
2015-10-09 16:14:45 +0800

    filter

--- 廖雪峰chapter9.py
+++ 廖雪峰chapter9.py
@@ -29,6 +29,7 @@ print(list(r))
 
 #reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算
 #reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
+#其中f必须有两个形参
 
 from functools import reduce
 def add_1(x, y):
@@ -47,7 +48,7 @@ print(result)
 def char2num(s):
 	return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
 
-result =  reduce(fn, map(char2num, '12345'))
+result = reduce(fn, map(char2num, '12345'))
 print(result)
 
 #lambda匿名函数
@@ -79,3 +80,56 @@ def fn_3(s):
 	return reduce(subFn, map(int, str1)) + reduce(subFn, map(int, str2)) / 10 ** len(str2)
 
 print(fn_3('123.45'))
+
+def fn_4(x, y):
+	return x * x + y * y
+print(reduce(fn_4, [1, 2, 3]))
+print(reduce(lambda x, y : x * x + y * y, [1, 2, 3]))
+
+#filter
+#filter用于过滤序列,filter()也接收一个函数和一个序列,filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素。
+
+def is_odd(n):
+	return n % 2 == 1
+print(filter(is_odd, [1, 2, 3, 4, 5, 6]))
+print(list(filter(is_odd, [1, 2, 3, 4, 5, 6])))
+
+def not_empty(s):
+	return s and s.strip()
+print(list(filter(not_empty, ['A', 'B', None])))
+
+print('=============')
+
+def _odd_iter():
+	n = 1
+	while True:
+		n += 2
+		yield n
+		
+def _not_divisible(n):
+	def fun(x):
+		return x % n > 0
+	return fun
+	
+def primes():
+	yield 2
+	it = _odd_iter()
+	while True:
+		n = next(it)
+		yield n
+		it = filter(_not_divisible(n), it) #此处虽然传递了参数n但是返回的还是函数名称
+		
+for n in primes():
+	if n < 100:
+		print(n)
+	else:
+		break
+		
+#练习-回数
+
+def is_palindrome(n):
+	s = str(n)
+	s1 = s[::-1]
+	return s == s1
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
